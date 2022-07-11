762bfda9678e41de06e1ba5156530893b8c703e1
vandana <vandana.kuncham@adyen.com>
2022-07-11 15:48:08 -0700

    pushing adyenPaymentMethod LWC and controller to the repo

---
+++ adyen-appexchange/main/default/lwc/adyenPaymentMethod/constants.js
@@ -0,0 +1,11 @@
+export const PaymentTypeEnum = Object.freeze({
+    PONUMBER: 'PurchaseOrderNumber',
+    CARDPAYMENT: 'CardPayment'
+});
+
+export const labels = {
+    paymentMethodHeader: 'Payment Method',
+    purchaseOrderOptionLabel: 'Purchase Order',
+    purchaseOrderEntryHeader: 'Enter PO Number',
+    cardPaymentOptionLabel: 'Credit Card'
+};
