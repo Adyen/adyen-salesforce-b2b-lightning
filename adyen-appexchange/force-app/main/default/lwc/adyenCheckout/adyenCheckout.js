aefe931d10643d602d7e0d29cdd1c8380e0b14df
Danilo Cardoso <cardoso.nilo@gmail.com>
2023-12-21 10:06:21 +0100

    Feature: SFI 515 implement payments call (#16)
    * feat: implement payment call plus unit tests
    
    * fix: unused variables
    
    * fix: deleting unused classes
    
    * feat: calling payments from checkout LWC
    
    * feat: setting psp reference
    
    * feat: applying review - string constants and env variable
    
    ---------
    
    Co-authored-by: daniloc <danilo.cardoso@adyen.com>

--- adyen-appexchange/force-app/main/default/lwc/adyenCheckout/adyenCheckout.js
+++ adyen-appexchange/force-app/main/default/lwc/adyenCheckout/adyenCheckout.js
@@ -5,55 +5,127 @@
 import {api, LightningElement} from 'lwc';
 import adyenCheckoutCSS from '@salesforce/resourceUrl/AdyenCheckoutCSS';
 import adyenCheckoutJS from '@salesforce/resourceUrl/AdyenCheckoutJS';
-import fetchPaymentMethods from "@salesforce/apex/AdyenDropInController.fetchPaymentMethods";
+import fetchPaymentMethods from '@salesforce/apex/AdyenDropInController.fetchPaymentMethods';
+import makePayment from '@salesforce/apex/AdyenDropInController.makePayment';
+// import makeDetailsCall from '@salesforce/apex/AdyenDropInController.makeDetailsCall';
 import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
 import { useCheckoutComponent } from 'commerce/checkoutApi';
+import userLocale from '@salesforce/i18n/locale';
+import { NavigationMixin } from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class AdyenCheckoutComponent extends useCheckoutComponent(LightningElement) {
+export default class AdyenCheckoutComponent extends useCheckoutComponent(NavigationMixin(LightningElement)) {
     static renderMode = 'light';
-    loading = true;
-    error = false;
+    @api adyenAdapter
+    @api environment;
     @api checkoutDetails;
     @api checkoutAddresses;
-    @api adyenAdapter
+    loading = true;
+    error = false;
     paymentMethods;
+    clientKey;
+    orderReferenceNumber;
+    cardEndDigits;
+
     connectedCallback() {
-        fetchPaymentMethods({adyenAdapterName: this.adyenAdapter}).then((result) => {
-            console.log('Result from fetch payment methods');
-            console.log(JSON.stringify(result));
-            this.paymentMethods = JSON.parse(result);
-            Promise.all([
-                loadStyle(this, adyenCheckoutCSS),
-                loadScript(this, adyenCheckoutJS),
-            ]).then(() => {
-                console.log('All scripts loaded');
-                try {
-                    AdyenCheckout(this.createConfigObject(this.paymentMethods)).then( checkout => {
-                        console.log('Result from checkout init')
-                        console.log(checkout);
-                        // Create an instance of Drop-in and mount it to the container you created.
-                        checkout.create('dropin').mount('#dropin-container');
-                    }).catch(error => {
-                        console.error('Error from checkout init')
-                        console.error(error);
-                        this.error = error;
-                    });
-                } catch (error) {
-                    console.error('Error from while mounting the drop in');
-                    console.error(JSON.stringify(error));
-                    this.error = error;
-                }
-            }).catch((error) => {
-                console.error('Error from loading scripts');
-                console.error(JSON.stringify(error));
-                this.error = error;
-            });
-        }).catch((error) => {
-            console.error('Error from fetch payment methods');
-            console.error(JSON.stringify(error));
-            this.error = error;
-        }).finally(() => {
+        this.loading = true;
+        this.fetchAdyenPaymentMethods();
+    }
+
+    async fetchAdyenPaymentMethods() {
+        try {
+            const paymentInfo = await fetchPaymentMethods({ adyenAdapterName: this.adyenAdapter });
+            this.handlePaymentInfo(paymentInfo);
+            await this.loadAdyenScripts();
+            await this.mountAdyenDropIn();
+        } catch (error) {
+            this.handleError(error);
+        } finally {
             this.loading = false;
+        }
+    }
+
+    handlePaymentInfo(paymentInfo) {
+        this.paymentMethods = JSON.parse(paymentInfo.paymentMethodsResponse);
+        this.clientKey = paymentInfo.clientKey;
+    }
+
+    async loadAdyenScripts() {
+        await Promise.all([
+            loadStyle(this, adyenCheckoutCSS),
+            loadScript(this, adyenCheckoutJS),
+        ]);
+    }
+
+    async mountAdyenDropIn() {
+        const checkout = await AdyenCheckout(this.createConfigObject(this.paymentMethods));
+        const dropin = checkout.create('dropin').mount('#dropin-container');
+    }
+
+    handleError(error) {
+        console.error(error);
+        this.error = error;
+    }
+
+    showConfirmationPage(placeOrderResult) {
+        const orderPageRef = {
+            type: 'comm__namedPage',
+            attributes: {
+                name: 'Order'
+            }
+        };
+        orderPageRef.state = {
+            orderNumber: placeOrderResult.orderReferenceNumber
+        };
+        this[NavigationMixin.Navigate](orderPageRef);
+    }
+
+    createConfigObject(paymentMethods) {
+        return {
+            paymentMethodsResponse: paymentMethods,
+            clientKey: this.clientKey,
+            locale: userLocale,
+            environment: this.environment,
+            onSubmit: async (state, dropin) => {
+                try {
+                    this.loading = true;
+                    const paymentResponse = await makePayment({
+                        paymentMethodType: state.data.paymentMethod.type,
+                        paymentMethod: JSON.stringify(state.data.paymentMethod),
+                        adyenAdapterName: this.adyenAdapter,
+                        endDigits: this.cardEndDigits
+                    });
+                    if (paymentResponse) {
+                        const placeOrderResult = await this.dispatchPlaceOrderAsync();
+                        this.showConfirmationPage(placeOrderResult);
+                    } else {
+                        this.showToast('Not authorized', 'Payment was not authorized, try again', 'error');
+                    }
+                } catch (error) {
+                    this.handleError(error);
+                } finally {
+                    this.loading = false;
+                }
+            },
+            paymentMethodsConfiguration: {
+                card: {
+                    hasHolderName: true,
+                    holderNameRequired: true,
+                    hideCVC: false,
+                    onFieldValid: (data) => {
+                        this.cardEndDigits =  data.endDigits ? data.endDigits : this.cardEndDigits;
+                    },
+                }
+            }
+        };
+    }
+
+    showToast(title, message, variant) {
+        const event = new ShowToastEvent({
+            title: title,
+            variant: variant,
+            message: message,
         });
+        this.dispatchEvent(event);
     }
 }
