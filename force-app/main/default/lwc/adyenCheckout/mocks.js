2ae5c330588d693da3c47dc2f15817423fb1ae1c
Danilo Cardoso <cardoso.nilo@gmail.com>
2024-06-10 13:24:51 +0200

    feat: new release configuration files (#45)
    Co-authored-by: daniloc <danilo.cardoso@adyen.com>

--- force-app/main/default/lwc/adyenCheckout/mocks.js
+++ force-app/main/default/lwc/adyenCheckout/mocks.js
@@ -1,4 +1,7 @@
-export const paymentMethodMockResponse = "{\"paymentMethods\": [" +
-        "{\"name\": \"Cards\", \"type\": \"scheme\", \"brands\": [\"visa\", \"mc\", \"amex\", \"discover\", \"diners\"]}," +
-        "{\"name\": \"ACH Direct Debit\", \"type\": \"ach\"}" +
-"]}";
+const paymentMethodMockResponse = "{\"paymentMethods\": [" +
+    "{\"name\": \"Cards\", \"type\": \"scheme\", \"brands\": [\"visa\", \"mc\", \"amex\", \"discover\", \"diners\"]}," +
+    "{\"name\": \"ACH Direct Debit\", \"type\": \"ach\"}," +
+    "{\"name\": \"SEPA Direct Debit\",\"type\": \"sepadirectdebit\"}" +
+    "]}";
+
+export { paymentMethodMockResponse };
