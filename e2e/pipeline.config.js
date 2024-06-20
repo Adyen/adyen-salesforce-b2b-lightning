663195dbe4aeaf4b87ad1c78a9f347e333346195
Cenk Kucukiravul <cenkiravul35@gmail.com>
2024-06-20 13:45:37 +0200

    Remove retry, add base url

--- e2e/pipeline.config.js
+++ e2e/pipeline.config.js
@@ -17,7 +17,7 @@ module.exports = defineConfig({
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0,
+	retries: 1,
 	/* Opt out of parallel tests on CI. */
 	workers: 1,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -25,7 +25,7 @@ module.exports = defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		// baseURL: 'http://127.0.0.1:3000',
+		baseURL: process.env.STORE_BASE_URL,
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
