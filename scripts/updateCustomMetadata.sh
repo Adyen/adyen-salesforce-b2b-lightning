4d04532e4e1ed0a1e95c3e54c093557254533069
shubhamk <shubham.kumar@adyen.com>
2024-07-17 14:03:22 +0200

    added script to update custom metadata

---
+++ scripts/updateCustomMetadata.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Exit script on error
+set -e
+
+# Path to the XML file
+XML_FILE="force-app/main/default/customMetadata/Adyen_Adapter.AdyenDefault.md-meta.xml"
+
+# Update the XML file
+xmlstarlet ed -L \
+    -u '//_:values[_:field="Merchant_Account__c"]/_:value' -v "$MERCHANT_ACCOUNT" \
+    -u '//_:values[_:field="Client_Key__c"]/_:value' -v "$CLIENT_KEY" \
+    -u '//_:values[_:field="Package_Namespace__c"]/_:value' -v "$PACKAGE_NAMESPACE" \
+    -u '//_:values[_:field="HMAC_Key__c"]/_:value' -v "$HMAC_KEY" \
+    "$XML_FILE"
+
+echo "customMetadata update complete."
