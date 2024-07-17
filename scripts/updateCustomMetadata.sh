8c0c6d5a6e909da7101e7f9a80049855e82eb488
shubhamk <shubham.kumar@adyen.com>
2024-07-17 14:14:08 +0200

    pipeline fix

--- scripts/updateCustomMetadata.sh
+++ scripts/updateCustomMetadata.sh
@@ -10,8 +10,8 @@ XML_FILE="force-app/main/default/customMetadata/Adyen_Adapter.AdyenDefault.md-me
 xmlstarlet ed -L \
     -u '//_:values[_:field="Merchant_Account__c"]/_:value' -v "$MERCHANT_ACCOUNT" \
     -u '//_:values[_:field="Client_Key__c"]/_:value' -v "$CLIENT_KEY" \
-    -u '//_:values[_:field="Package_Namespace__c"]/_:value' -v "$PACKAGE_NAMESPACE" \
     -u '//_:values[_:field="HMAC_Key__c"]/_:value' -v "$HMAC_KEY" \
+    -u '//_:values[_:field="Package_Namespace__c"]/_:value' -v "" \
     "$XML_FILE"
 
 echo "customMetadata update complete."
